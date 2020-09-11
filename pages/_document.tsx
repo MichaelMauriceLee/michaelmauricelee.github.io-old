c31461b2f46fb3f19d0055c2130e2657cb8f93c6
Michael Lee <m.maurice.lee@gmail.com>
2020-09-10 20:34:29 -0600

    Fixed PWA installability

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -38,11 +38,39 @@ class MyDocument extends Document {
       <html lang="en" dir="ltr">
         <Head>
           <meta charSet="utf-8" />
+          <meta name="application-name" content="Michael Lee's Portfolio" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-title" content="ML's Portfolio" />
+          <meta name="description" content="Best PWA App in the world" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="msapplication-config" content="/static/icons/browserconfig.xml" />
+          <meta name="msapplication-TileColor" content="#2B5797" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#000000" />
+          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover" />
+
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/icons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon.ico" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon.ico" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:url" content="https://yourdomain.com" />
+          <meta name="twitter:title" content="PWA App" />
+          <meta name="twitter:description" content="Best PWA App in the world" />
+          <meta name="twitter:image" content="https://yourdomain.com/static/icons/android-chrome-192x192.png" />
+          <meta name="twitter:creator" content="@DavidWShadow" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="PWA App" />
+          <meta property="og:description" content="Best PWA App in the world" />
+          <meta property="og:site_name" content="PWA App" />
+          <meta property="og:url" content="https://yourdomain.com" />
+          <meta property="og:image" content="https://yourdomain.com/static/icons/apple-touch-icon.png" />
           {/* PWA primary color */}
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
+
         </Head>
         <body>
           <Main />
