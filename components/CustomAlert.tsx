3b2d8c73fed02add39246cebf7d9d55cbd9dfb64
Michael Lee <m.maurice.lee@gmail.com>
2020-09-24 08:12:59 -0600

    Added snackbar alert

--- components/CustomAlert.tsx
+++ components/CustomAlert.tsx
@@ -4,6 +4,7 @@ import { Alert, Color } from '@material-ui/lab';
 import Slide from '@material-ui/core/Slide';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
+import { Snackbar } from '@material-ui/core';
 
 interface IProps {
   severity: Color;
@@ -14,26 +15,41 @@ interface IProps {
 
 const CustomAlert: React.FC<IProps> = ({
   severity, message, displayAlert, setDisplayAlert,
-}) => (
-  <Slide in={displayAlert}>
-    <Alert
-      severity={severity}
-      action={(
-        <IconButton
-          aria-label="close"
-          color="inherit"
-          size="small"
-          onClick={() => {
-            setDisplayAlert(false);
-          }}
-        >
-          <CloseIcon fontSize="inherit" />
-        </IconButton>
-          )}
+}) => {
+  const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setDisplayAlert(false);
+  };
+  return (
+    <Snackbar
+      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      open={displayAlert}
+      autoHideDuration={6000}
+      onClose={handleClose}
+      TransitionComponent={Slide}
     >
-      {message}
-    </Alert>
-  </Slide>
-);
+      <Alert
+        severity={severity}
+        action={(
+          <IconButton
+            aria-label="close"
+            color="inherit"
+            size="small"
+            onClick={() => {
+              setDisplayAlert(false);
+            }}
+          >
+            <CloseIcon fontSize="inherit" />
+          </IconButton>
+        )}
+      >
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+};
 
 export default CustomAlert;
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -10,6 +10,7 @@ import { Color } from '@material-ui/lab';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Paper from '@material-ui/core/Paper';
 import CustomAlert from '../components/CustomAlert';
 import NavBar from '../components/NavBar';
 import Link from '../components/Link';
@@ -31,23 +32,19 @@ interface IFormValueErrors {
 const StyledApp = styled.div`
   margin: 0;
   padding: 0;
+  min-height: 100vh;
   font-family: 'Verdana', 'san serif';
+  overflow:auto;
   display: flex;
   flex-direction: column;
-  height: 100vh;
-  background-color: black;
+  background-color: #6c757d;
 `;
 
 const StyledPage = styled.div`
   display: flex;
-  flex-direction: column;
-  text-align: center;
-  justify-content: center;
-  align-items: center;
-  flex: 1;
-  height: 100%;
   width: 80%;
   margin: auto;
+  flex: 1;
 `;
 
 const StyledMenuIcon = styled.div`
@@ -56,13 +53,28 @@ const StyledMenuIcon = styled.div`
   left: 0.5%;
 `;
 
+const StyledContainer = styled(Paper)`
+  && {
+    background-color: #343a40;
+  }
+  position: relative;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+`;
+
 const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
   text-align: start;
+  background-color: grey;
   height: 100%;
   width: 100%;
-  background-color: grey;
+`;
+
+const StyledAlert = styled(CustomAlert)`
+  position: absolute;
 `;
 
 const Contact: React.FC = () => {
@@ -113,92 +125,95 @@ const Contact: React.FC = () => {
               <MenuIcon style={{ fill: 'white' }} />
             </IconButton>
           </StyledMenuIcon>
+          <StyledAlert
+            severity={severity}
+            message={message}
+            displayAlert={displayAlert}
+            setDisplayAlert={setDisplayAlert}
+          />
           <StyledPage>
-            <CustomAlert
-              severity={severity}
-              message={message}
-              displayAlert={displayAlert}
-              setDisplayAlert={setDisplayAlert}
-            />
-            <Form
-              onSubmit={sendEmail}
-              validate={validate}
-              render={({
-                handleSubmit, form, pristine, invalid, submitting,
-              }) => (
-                <StyledForm onSubmit={async (event) => {
-                  try {
-                    await handleSubmit(event);
-                    setSeverity('success');
-                    setMessage('Email successfully sent!');
-                    // @ts-ignore
-                    form.restart();
-                  } catch (err) {
-                    setSeverity('error');
-                    setMessage(err.text);
-                  } finally {
-                    setDisplayAlert(true);
-                    setTimeout(() => {
-                      setDisplayAlert(false);
-                    }, 3000);
-                  }
-                }}
-                >
-                  {submitting && <CircularProgress />}
-                  <Field
-                    name="name"
-                    render={({ input, meta }) => (
-                      <TextField
-                        label="Name"
-                        color="primary"
-                        helperText={meta.touched ? meta.error : undefined}
-                        error={meta.error && meta.touched}
-                        {...input}
-                      />
-                    )}
-                  />
-                  <Field
-                    name="email"
-                    render={({ input, meta }) => (
-                      <TextField
-                        label="Email"
-                        color="primary"
-                        helperText={meta.touched ? meta.error : undefined}
-                        error={meta.error && meta.touched}
-                        {...input}
-                      />
-                    )}
-                  />
-                  <Field
-                    name="subject"
-                    render={({ input, meta }) => (
-                      <TextField
-                        label="Subject"
-                        color="primary"
-                        helperText={meta.touched ? meta.error : undefined}
-                        error={meta.error && meta.touched}
-                        {...input}
-                      />
-                    )}
-                  />
-                  <Field
-                    name="message"
-                    render={({ input, meta }) => (
-                      <TextField
-                        label="Message"
-                        color="primary"
-                        multiline
-                        rows={4}
-                        helperText={meta.touched ? meta.error : undefined}
-                        error={meta.error && meta.touched}
-                        {...input}
-                      />
-                    )}
-                  />
-                  <Button type="submit" disabled={pristine || invalid || submitting}>Submit</Button>
-                </StyledForm>
-              )}
-            />
+            <StyledContainer>
+              <Form
+                onSubmit={sendEmail}
+                validate={validate}
+                render={({
+                  handleSubmit, form, pristine, invalid, submitting,
+                }) => (
+                  <StyledForm onSubmit={async (event) => {
+                    try {
+                      await handleSubmit(event);
+                      setSeverity('success');
+                      setMessage('Email successfully sent!');
+                      // @ts-ignore
+                      form.restart();
+                    } catch (err) {
+                      setSeverity('error');
+                      setMessage(err.text);
+                    } finally {
+                      setDisplayAlert(true);
+                      setTimeout(() => {
+                        setDisplayAlert(false);
+                      }, 3000);
+                    }
+                  }}
+                  >
+                    {submitting && <CircularProgress />}
+                    <Field
+                      name="name"
+                      render={({ input, meta }) => (
+                        <TextField
+                          label="Name"
+                          color="primary"
+                          helperText={meta.touched ? meta.error : undefined}
+                          error={meta.error && meta.touched}
+                          {...input}
+                        />
+                      )}
+                    />
+                    <Field
+                      name="email"
+                      render={({ input, meta }) => (
+                        <TextField
+                          label="Email"
+                          color="primary"
+                          helperText={meta.touched ? meta.error : undefined}
+                          error={meta.error && meta.touched}
+                          {...input}
+                        />
+                      )}
+                    />
+                    <Field
+                      name="subject"
+                      render={({ input, meta }) => (
+                        <TextField
+                          label="Subject"
+                          color="primary"
+                          helperText={meta.touched ? meta.error : undefined}
+                          error={meta.error && meta.touched}
+                          {...input}
+                        />
+                      )}
+                    />
+                    <Field
+                      name="message"
+                      render={({ input, meta }) => (
+                        <TextField
+                          label="Message"
+                          color="primary"
+                          multiline
+                          rows={4}
+                          helperText={meta.touched ? meta.error : undefined}
+                          error={meta.error && meta.touched}
+                          {...input}
+                        />
+                      )}
+                    />
+                    <Button type="submit" disabled={pristine || invalid || submitting}>Submit</Button>
+                  </StyledForm>
+                )}
+              />
+
+            </StyledContainer>
           </StyledPage>
         </StyledApp>
       </main>
